c566d2fec0b80133e820986b5e4657312dbb21b2
Sawon-52 <mehedihasansawon.52@gmail.com>
2024-03-17 23:34:49 +0600

    many way to conditional rendering

--- src/Todo.jsx
+++ src/Todo.jsx
@@ -6,12 +6,40 @@
 //   );
 // }
 
-
 // conditional rendaring
+// export default function Todo({ task, isDone }) {
+//   if (isDone) {
+//     return <li>Finisd: {task}</li>;
+//   } else {
+//     return <li>Work On: {task}</li>;
+//   }
+// }
+
+// conditional rendering option 02 : ternany oparetor
+// export default function Todo({ task, isDone }) {
+//   return (
+//     <li>
+//       {isDone ? "Finished" : "Work on"} {task}
+//     </li>
+//   );
+// }
+
+// conditional rendering option 03 : && oparetor
+// export default function Todo({ task, isDone }) {
+//   return (
+//     <li>
+//       {task}
+//       {isDone && ": Done"}
+//     </li>
+//   );
+// }
+
+// conditional rendering option 03 : || oparetor
 export default function Todo({ task, isDone }) {
-  if (isDone) {
-    return <li>Finisd: {task}</li>;
-  } else {
-    return <li>Work On: {task}</li>;
-  }
+  return (
+    <li>
+      {task}
+      {isDone || ": Do it"}
+    </li>
+  );
 }
